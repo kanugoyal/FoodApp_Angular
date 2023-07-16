e0e9a77510c9baf10e60719bebe1dff0c09822a9
Kanika <goyalkanu25@gmail.com>
2023-07-16 23:20:27 +0530

    header html and css

---
+++ frontend/src/data.ts
@@ -0,0 +1,114 @@
+import { Food } from "./app/shared/models/food";
+
+export const sample_foods: Food[] = [
+    {
+        id:'1',
+        name:'Golden Corn Pizza',
+        price: 129,
+        cookTime:'30-40',
+        origins: ['italy'],
+        
+        imageUrl: '/home/kanika/Desktop/foodwine/frontend/src/assets/goldenCorn.jpeg',
+        tags: ['FastFood', 'Pizza','Snacks'],
+        stars: 4.1,
+    },
+    {
+        id:'2',
+        name:'Double Cheese Pizza',
+        price: 199,
+        cookTime:'40-50',
+        origins: ['italy'],
+        
+        imageUrl: 'frontend/src/assets/DoubleCheese.jpeg',
+        tags: ['FastFood', 'Pizza','Snacks'],
+        stars: 4.3,
+    },
+    {
+        id:'3',
+        name:'Chilliy Momos ',
+        price: 99,
+        cookTime:'30-40',
+        origins: ['korean'],
+        
+        imageUrl: 'frontend/src/assets/chillyMomos.jpeg',
+        tags: ['FastFood', 'Momos','Snacks'],
+        stars: 4.6,
+    },
+    {
+        id:'4',
+        name:'Pasta',
+        price: 119,
+        cookTime:'30-40',
+        origins: ['italy'],
+        
+        imageUrl: 'frontend/src/assets/pasta.jpeg',
+        tags: ['FastFood', 'Pasta','Snacks'],
+        stars: 4.7,
+    },
+    {
+        id:'5',
+        name:'Chinese Cuisine',
+        price: 399,
+        cookTime:'50-60',
+        origins: ['chinese'],
+        
+        imageUrl: '/home/kanika/Desktop/foodwine/frontend/src/assets/goldenCorn.jpeg',
+        tags: ['FastFood', 'Noodles','manchurian','fried rice','Snacks'],
+        stars: 4.5,
+    },
+    {
+        id:'6',
+        name:'Spring Rolls',
+        price: 99,
+        cookTime:'30-40',
+        origins: ['filipino'],
+        
+        imageUrl: '/frontend/src/assets/springRoll.jpeg',
+        tags: ['FastFood', 'Pizza','Snacks'],
+        stars: 4.1,
+    },
+    {
+        id:'7',
+        name:'Manchurian',
+        price: 129,
+        cookTime:'30-40',
+        origins: ['China'],
+        
+        imageUrl: 'frontend/src/assets/manchurian.jpeg',
+        tags: ['FastFood', 'manchurian','Snacks'],
+        stars: 4.1,
+    },
+    {
+        id:'8',
+        name:'Masala Dosa',
+        price: 129,
+        cookTime:'30-40',
+        origins: ['india'],
+        
+        imageUrl: 'frontend/src/assets/dosa.jpeg',
+        tags: ['FastFood', 'Dosa','South indian','Snacks'],
+        stars: 4.1,
+    },
+    {
+        id:'9',
+        name:'Bada Pav',
+        price: 29,
+        cookTime:'30-40',
+        origins: ['india'],
+        
+        imageUrl: 'frontend/src/assets/bdapav.jpeg',
+        tags: ['FastFood', 'Bda pav','Snacks'],
+        stars: 4.1,
+    },
+    {
+        id:'10',
+        name:'Samosa',
+        price: 30,
+        cookTime:'30-40',
+        origins: ['india'],
+        
+        imageUrl: 'frontend/src/assets/samosa.jpeg',
+        tags: ['FastFood', 'samosa','Snacks'],
+        stars: 4.1,
+    }
+]
